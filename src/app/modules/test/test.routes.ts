e2d58aa066f3a2f09d68d3517a12353aa2cddc90
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-06-06 15:56:11 +0600

    removed test module for testing purpose

--- src/app/modules/test/test.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { testControllers } from './test.controllers';
-
-const router = Router();
-
-router.get('/', testControllers.getAllTests);
-
-export const testRoutes = router;
--- src/app/modules/test/test.validation.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-
-const creationSchema = z.object({}).strict();
-
-export const testValidations = { creationSchema };
